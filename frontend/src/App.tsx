caed603149709e96c6c15ca07c5a66b7d119db02
LucasdaCostaSilva <landi_cnt@yahoo.com>
2022-01-10 23:34:15 -0300

    Form layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,17 @@ import {
   Route
 } from "react-router-dom";
 import Navbar from "components/Navbar";
+import Form from "pages/Form";
+import Listing from "pages/Listing";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<br />} />
+        <Route path="/" element={<Listing />} />
         <Route path="/form">
-          <Route path=":movieId" element={<br />} />
+          <Route path=":movieId" element={<Form />} />
         </Route>
       </Routes>
     </BrowserRouter>
