701c2aa71cf86532764b13e646006cf0fe462802
LucasdaCostaSilva <landi_cnt@yahoo.com>
2022-01-12 22:16:04 -0300

    Props

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,14 +1,19 @@
-import {ReactComponent as Arrow} from 'assets/img/arrow.svg';
+import { ReactComponent as Arrow } from 'assets/img/arrow.svg';
 import './styles.css';
 
-function Pagination() {
+interface props {
+  pagina: number;
+  total: number;
+}
+
+function Pagination({ pagina, total }: props) {
   return (
     <div className="dsmovie-pagination-container">
       <div className="dsmovie-pagination-box">
         <button className="dsmovie-pagination-button" disabled={true} >
           <Arrow />
         </button>
-        <p>{`${1} de ${3}`}</p>
+        <p>{`${pagina} de ${total}`}</p>
         <button className="dsmovie-pagination-button" disabled={false} >
           <Arrow className="dsmovie-flip-horizontal" />
         </button>
